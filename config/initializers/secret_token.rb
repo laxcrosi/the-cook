77b7f8f6e4188e25fa3128ad59853c1085ff949f
Laxcrosi <laxcrosi@gmail.com>
2014-06-17 16:25:22 -0600

    Add Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheCook::Application.config.secret_key_base = 'd9cd25cb9d5930352780fc4dedbbc167f7ca38dbad0c7ffdef31cbc56ef5d0842b4ef031aba77c283a55285381b6f80f6304c3cc37595c5c9c16f42714ff5b27'
